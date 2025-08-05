df60f19bf9434eacb4abd7799faa93196862428f
Seykes <alakor991@gmail.com>
2025-08-05 04:15:27 +0300

    ft: remove some triggers

--- shell_injection.py
+++ shell_injection.py
@@ -1,11 +1,11 @@
-# CWE-78: OS Command Injection
+# # CWE-78: OS Command Injection
 
-import os
+# import os
 
-def ping_host():
-    host = input("Enter host to ping: ")
+# def ping_host():
+#     host = input("Enter host to ping: ")
     
-    # Shell Injection (Bandit & Semgrep)
-    os.system(f"ping -c 4 {host}")  
+#     # Shell Injection (Bandit & Semgrep)
+#     os.system(f"ping -c 4 {host}")  
 
-ping_host()
+# ping_host()
--- sql_injection.py
+++ sql_injection.py
@@ -1,20 +1,20 @@
-# CWE-89: SQL Injection
+# # CWE-89: SQL Injection
 
-import sqlite3
+# import sqlite3
 
-def get_user_input():
-    user_input = input("Enter username: ")
-    return user_input
+# def get_user_input():
+#     user_input = input("Enter username: ")
+#     return user_input
 
-def query_db(username):
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
+# def query_db(username):
+#     conn = sqlite3.connect("users.db")
+#     cursor = conn.cursor()
     
-    # SQL Injection (Bandit & Semgrep)
-    query = f"SELECT * FROM users WHERE username = '{username}'"
-    cursor.execute(query)
+#     # SQL Injection (Bandit & Semgrep)
+#     query = f"SELECT * FROM users WHERE username = '{username}'"
+#     cursor.execute(query)
     
-    return cursor.fetchall()
+#     return cursor.fetchall()
 
-user = get_user_input()
-print(query_db(user))
+# user = get_user_input()
+# print(query_db(user))
